86c75ce4358deeb9845c6d123a961cac94172bf0
gmarik <gmarik@gmail.com>
2011-10-18 16:43:27 -0500

    basic login spec

---
+++ spec/integration/login_spec.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+describe 'Logging integration' do
+  it 'logs you in with valid user/pass' do
+    visit '/'
+    fill_in 'session_login', with: 'autotest'
+    fill_in 'session_password', with: 'autotest'
+    click_button 'Log in'
+    within "#flashes" do
+      page.must_be :has_content?, 'Logged in successfully'
+    end
+  end
+end
+
